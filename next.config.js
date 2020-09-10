262ae3205e15a73d98d5e682d51855ed45342d90
Andrew Aylett <andrew.aylett@skyscanner.net>
2020-09-10 20:40:20 +0100

    Use SCSS for styles

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    webpack(config, { dev }) {
+        if (!dev) {
+            config.devtool = 'source-map'
+            for (const plugin of config.plugins) {
+                if (plugin['constructor']['name'] === 'UglifyJsPlugin') {
+                    plugin.options.sourceMap = true
+                    break
+                }
+            }
+        }
+        return config
+    },
+}
