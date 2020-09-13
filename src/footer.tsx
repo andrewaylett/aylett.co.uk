ee27f5326fc8aa8f205f3f60ba25f330b31ef5f2
Andrew Aylett <andrew.aylett@skyscanner.net>
2020-09-13 20:53:52 +0100

    Failed to type-check properly :P.
    This is a prop-types issue -- I really should integrate it properly.

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getYear } from 'date-fns'
 
-const Footer = ({ author, copyright }) => (
+const Footer: React.FunctionComponent<{ author?: string; copyright?: string }> = ({ author, copyright }) => (
   <footer style={{ paddingTop: '1em' }}>
     Copyright © {author || 'Andrew Aylett'}, {copyright || getYear(Date.now())}
   </footer>
