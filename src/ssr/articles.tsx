5da1db1dcf049595f1e2d26873cdfebbdf15c4b7
Andrew Aylett <andrew.aylett@skyscanner.net>
2020-10-20 16:07:43 +0100

    Move SSR-only code into a subfolder
    This allows me to lint code that will only run on the server for current node, while linting code that might run on the client for all supported browsers.

---
+++ src/ssr/articles.tsx
@@ -0,0 +1,31 @@
+import { GetStaticProps } from 'next'
+import path from 'path'
+import fs from 'fs-extra'
+import sort_by from '../sort_by'
+
+type Page = { name: string; metadata: any }
+
+export const getStaticProps: GetStaticProps<{ pages: Page[] }> = async () => {
+  const ARTICLES_PATH = path.join(process.cwd(), 'src', 'pages', 'articles')
+  const items = await fs.readdir(ARTICLES_PATH)
+  const promises: Promise<Page[]>[] = items.map(async (item) => {
+    const filePath = path.join(ARTICLES_PATH, item)
+    const { ext, name } = path.parse(filePath)
+    // Only process markdown/mdx files that are not index.tsx pages
+    if (ext.startsWith('.md') && name !== 'index') {
+      const module = await import(`../pages/articles/${item}`)
+      return [{ name, metadata: module.metadata }]
+    }
+    return []
+  })
+
+  const entries: Page[] = (await Promise.all(promises)).flat(1)
+
+  const sorted: Page[] = sort_by(entries, (entry) => entry.metadata.title)
+
+  return {
+    props: {
+      pages: sorted,
+    },
+  }
+}
