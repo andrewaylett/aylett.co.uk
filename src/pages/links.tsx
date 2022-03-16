a3ec1a3f15228b05fa22429c69bc0f567eed9719
Andrew Aylett <andrew.aylett@skyscanner.net>
2022-03-16 12:32:20 +0000

    Add links page

---
+++ src/pages/links.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import React from 'react';
+import { InferGetStaticPropsType } from 'next';
+
+import styles from '../index.module.scss';
+
+// noinspection HtmlUnknownTarget
+export const Home: React.VoidFunctionComponent<InferGetStaticPropsType<void>> = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Niddrie Tech Rota</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main>
+      <h1 className={styles.title}>Links</h1>
+
+      <p className={styles.description}>
+        <a href="https://photos.app.goo.gl/tRwdQNpn5j15PKJJ7">Lizzie&apos;s Photos</a>
+      </p>
+    </main>
+  </div>
+);
+
+export default Home;
