5889d21b46591d4f6259c24768545a7d6d77abf4
Andrew Aylett <andrew@aylett.co.uk>
2022-10-30 20:29:02 +0000

    Fix CSP, fix head content

--- src/app/articles/head.tsx
+++ src/app/articles/head.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 // noinspection JSUnusedGlobalSymbols
 export default function Head(): React.ReactNode {
   return (
-    <head>
+    <>
       <title>Articles - aylett.co.uk</title>
       <link rel="icon" href="/favicon.ico" />
-    </head>
+    </>
   );
 }
--- src/app/schema/head.tsx
+++ src/app/schema/head.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 // noinspection JSUnusedGlobalSymbols
 export default function Head(): React.ReactNode {
   return (
-    <head>
+    <>
       <title>aylett.co.uk schemas</title>
       <link rel="icon" href="/favicon.ico" />
-    </head>
+    </>
   );
 }
