8607088fa3ed0af126ead697a1785b90dedf9a40
Andrew Aylett <andrew@aylett.co.uk>
2025-12-19 00:19:58 +0000

    Add a landing page for Home Assistant Desktop OAuth

---
+++ src/app/oauth-redirect/page.tsx
@@ -0,0 +1,38 @@
+import 'server-only';
+
+import React, { type ReactNode } from 'react';
+
+import { type Metadata } from 'next';
+
+import { PageStructure } from '@/components/PageStructure';
+
+export const metadata: Metadata = {
+  title: 'Home Assistant Desktop',
+  icons: {
+    other: {
+      rel: 'redirect_uri',
+      url: 'homeassistant://oauth-callback',
+    },
+  },
+};
+
+function Redirect(): ReactNode {
+  return (
+    <PageStructure
+      header={
+        <>
+          <h1 className="text-center">Home Assistant Desktop</h1>
+        </>
+      }
+      schemaType={''}
+      resource={''}
+    >
+      <p>
+        If you see this, it's probably because something has gone wrong with
+        logging into your Home Assistant instance.
+      </p>
+    </PageStructure>
+  );
+}
+
+export default Redirect;
