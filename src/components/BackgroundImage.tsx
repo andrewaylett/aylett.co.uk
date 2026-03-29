dfe3c7c6667160da8a0321ccf6d57a6b8875e786
Andrew Aylett <andrew@aylett.co.uk>
2026-03-29 15:31:09 +0100

    Use plain CSS to switch images, lazy loaded

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -7,37 +7,37 @@ import Image from 'next/image';
 import field from '../../public/PXL_20220610_201053680~2.jpg';
 import night from '../../public/PXL_20250407_202232580.NIGHT.jpg';
 
-import { useDarkMode } from '@/client/hooks/useDarkMode';
-
 export function BackgroundImage() {
-  const darkMode = useDarkMode();
   return (
     <div className="fixed w-full h-full -z-50">
-      {darkMode ? (
-        <Image
-          key="night"
-          src={night}
-          placeholder="blur"
-          alt=""
-          style={{
-            objectFit: 'cover',
-          }}
-          fill={true}
-          sizes="calc(max(100vw, 133vh))"
-        />
-      ) : (
-        <Image
-          key="day"
-          src={field}
-          placeholder="blur"
-          alt=""
-          style={{
-            objectFit: 'cover',
-          }}
-          fill={true}
-          sizes="calc(max(100vw, 133vh))"
-        />
-      )}
+      <Image
+        key="night"
+        src={night}
+        placeholder="blur"
+        alt=""
+        style={{
+          objectFit: 'cover',
+        }}
+        fill={true}
+        sizes="calc(max(100vw, 133vh))"
+        className="hidden dark:block"
+        loading="lazy"
+        fetchPriority="low"
+      />
+      <Image
+        key="day"
+        src={field}
+        placeholder="blur"
+        alt=""
+        style={{
+          objectFit: 'cover',
+        }}
+        fill={true}
+        sizes="calc(max(100vw, 133vh))"
+        className="block dark:hidden"
+        loading="lazy"
+        fetchPriority="low"
+      />
     </div>
   );
 }
