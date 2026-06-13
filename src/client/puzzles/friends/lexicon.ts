6a367df98ab2ee81ad52e93efd41c9e1e1de5c74
Andrew Aylett <andrew@aylett.co.uk>
2026-06-14 00:31:27 +0100

    Try to ensure words are present in production

--- src/client/puzzles/friends/lexicon.ts
+++ src/client/puzzles/friends/lexicon.ts
@@ -1,10 +1,12 @@
 import * as fs from 'node:fs/promises';
+import path from 'node:path';
 
 import {
   AVOID,
   buildPrefixes,
   DEFAULT_WORDS,
 } from '@/client/puzzles/friends/words';
+import { findProjectDirectory } from '@/remark/traverse';
 
 export interface Lexicon {
   words: string[];
@@ -23,7 +25,12 @@ export interface BoardCtx {
  * word, so each board can quickly extract only the words its letters allow.
  */
 async function makeLexicon(): Promise<Lexicon> {
-  const text = await fs.readFile('./words.txt', 'utf8');
+  const projectDirectory = await findProjectDirectory();
+  const wordsFile = path.join(
+    projectDirectory,
+    'src/client/puzzles/friends/words/words.txt',
+  );
+  const text = await fs.readFile(wordsFile, 'utf8');
 
   const set = new Set(DEFAULT_WORDS);
   for (const line of text.split(/\r?\n/)) {
