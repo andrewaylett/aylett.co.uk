8749c19834e49f3a88e3911206a1af13701ff87b
Andrew Aylett <andrew@aylett.co.uk>
2026-07-04 02:40:12 +0100

    Wrap puzzle in layout

--- src/app/puzzles/word-friends/page.tsx
+++ src/app/puzzles/word-friends/page.tsx
@@ -6,7 +6,6 @@ import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 
 import type { Metadata } from 'next';
 
-import { PageStructure } from '@/components/PageStructure';
 import { LineTraceWordGame } from '@/client/puzzles/friends/LineTraceWordGame';
 import { LineTraceErrorComponent } from '@/app/puzzles/word-friends/LineTraceErrorComponent';
 
@@ -19,22 +18,15 @@ export const metadata: Metadata = {
 
 export default function PuzzlesPage(): JSX.Element {
   return (
-    <PageStructure
-      breadcrumbs={[]}
-      header={<h1 className="text-center">{TITLE}</h1>}
-      schemaType="Item"
-      resource="/puzzles"
-    >
-      <main className="flex flex-col justify-center items-center">
-        <p>
-          Trace along the lines to find words of 4+ letters. Letters that are no
-          longer needed turn into friends; lines fade away when they&apos;re
-          spent.
-        </p>
-        <ErrorBoundary errorComponent={LineTraceErrorComponent}>
-          <LineTraceWordGame />
-        </ErrorBoundary>
-      </main>
-    </PageStructure>
+    <>
+      <p>
+        Trace along the lines to find words of 4+ letters. Letters that are no
+        longer needed turn into friends; lines fade away when they&apos;re
+        spent.
+      </p>
+      <ErrorBoundary errorComponent={LineTraceErrorComponent}>
+        <LineTraceWordGame />
+      </ErrorBoundary>
+    </>
   );
 }
